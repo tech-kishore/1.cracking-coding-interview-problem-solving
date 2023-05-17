364afa44a9d9811f623a0cfc264b0487e567d462
Kishore Rajkumar <tech.kishore@hotmail.com>
2023-05-17 13:35:25 +0530

    removed bottom variable

--- Stack&Queues/StackExample.java
+++ Stack&Queues/StackExample.java
@@ -2,18 +2,45 @@
 // Use this editor to write, compile and run your Java code online
 
 public class StackExample {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        
+        Stack s=new Stack();
+        s.push(100);
+        s.push(200);
+        s.push(300);
+        s.push(400);
+        s.push(500);
+        s.push(600);
+        
+        try{
+            System.out.printf("Pop 1: %d\n", s.pop());
+            System.out.printf("Pop 2: %d\n", s.pop());
+            System.out.printf("Peek 1: %d\n", s.peek());
+            System.out.printf("Pop 3: %d\n", s.pop());
+            System.out.printf("Peek 2: %d\n", s.peek());
+        }catch(Exception e){
+             System.out.println(e.getMessage());
+        }
     }
-    
 }
 
 class Stack{
-    private StackNode bottom;
     private StackNode top;
+    
+    public Stack(){
+    }
     
     public Stack(int data){
         top=new StackNode(data);
+    }
+    
+    private class StackNode{
+        int data;
+        StackNode next;
+        
+        StackNode(int _data){
+            this.data=_data;    
+        }
     }
     
     public boolean isEmpty(){
@@ -32,5 +59,18 @@ class Stack{
         top = newNode; // Make new node the current top
     }
     
-    // continue .....
+    public int pop() throws Exception{
+        if(isEmpty())
+            throw new Exception("Pop: No element");
+            
+        int value = top.data;
+        top=top.next; //remove top of data
+        return value;
+    }
+    
+    public int peek() throws Exception{
+        if(isEmpty()) 
+            throw new Exception("Peek: No element");
+        return top.data;
+    }
 }
