beaea19f0985eef57c9f9adf4c4add53e8543237
tech-kishore <124598875+tech-kishore@users.noreply.github.com>
2023-09-21 13:35:46 +0530

    Update GraphTraversal.java

--- GraphTraversal.java
+++ GraphTraversal.java
@@ -2,32 +2,40 @@
 // Use this editor to write, compile and run your Java code online
 class GraphTraversal {
     public static void main(String[] args) {
-        Graph graph = createGraph();
-        DFS(graph);
+        int len=6;
+        Graph graph = createGraph(len);
+        DFS(graph,0,len);
     }
     
-    private static void DFS(Graph g){
-        Graph.Node start = g.getVertex(0);
+    private static void DFS(Graph g,int idx,int len){
+        if(idx>=len)
+            return;
+        Graph.Node start = g.getVertex(idx);
+        System.out.printf("> Pnode = %s \n",start);
         search(start);
+        DFS(g,idx++,len);
     }
     
     private static void search(Graph.Node start){
-        System.out.println(start.visited);
         if(start==null) 
             return;
         // visit(start);
         start.visited=true;
+
         for(Graph.Node n:start.neighbors){
+            if(n==null){
+                continue;
+            }
             if(n.visited==false){
-                System.out.println(n);
+                System.out.printf(" \t\t - {Cnode = %s} \n",n);
                 search(n);
             }
         }
         
     }
     
-    private static Graph createGraph(){
-        Graph g = new Graph(6);
+    private static Graph createGraph(int len){
+        Graph g = new Graph(len);
         
         Graph.Node n1 = g.new Node("n1");
         Graph.Node n2 = g.new Node("n2");
@@ -35,14 +43,16 @@ class GraphTraversal {
         Graph.Node n4 = g.new Node("n4");
         Graph.Node n5 = g.new Node("n5");
         
-        n1.setNeighbor(n2);
-        n1.setNeighbor(n3);
         
         g.addVertex(n1);
         g.addVertex(n2);
         g.addVertex(n3);
         g.addVertex(n4);
         g.addVertex(n5);
+        
+        n1.setNeighbor(n2);
+        n1.setNeighbor(n3);
+        
         return g;
     }
 }
@@ -68,6 +78,9 @@ class Graph{
             neighbors[last++] = n;
         }
         
+        public String toString(){
+            return name;
+        }
         
     }
     
